6dfe0f3a566734934ca34db5d08e22475d0ed37a
Beirão <nandabeiraose@gmail.com>
2025-01-08 20:44:45 -0300

    Create ForumHubApplicationTests.java

---
+++ src/test/java/com/forumhub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Este teste verifica se o contexto da aplicação carrega corretamente
+    }
+}
